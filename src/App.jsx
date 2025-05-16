9b581849097f75c9f1bf84faf11d54a770d0a84d
nonbinarybyte <darleenfairy33@myyahoo.com>
2025-05-16 13:41:16 -0400

    2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { Card, CardContent } from './components/ui/card';
 import { Navbar } from './components/ui/navbar';
 import { AppLayout } from './components/ui/applayout';
 import { Button } from './components/ui/button';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
+import { useAnalytics } from '@vercel/analytics/react';
 import { motion } from 'framer-motion';
 import './App.css';
 
