7f18bebab7a5f836fc6e52591dceada23766dd67
Kenny | 肯尼 <darleenfairy33@myyahoo.com>
2025-06-11 20:38:48 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/nonbinarybyte.com/',
     plugins: [react()],
     build: {
         rollupOptions: {
